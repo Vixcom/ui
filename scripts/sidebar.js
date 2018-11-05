89aede27b90b96f8ed1feed097bfcf627edf16f1
vixson <vixsondj72@gmail.com>
2018-11-05 11:43:01 +0100

    Fixed: CLose on outside click.
    1. Also removed sidebar-container classList observable.
    
    2. Updated css structure and added few css effects.
    
    3. Hacked out close on outside click.
    
    Nextup: Close on nav-item clicked

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -3,31 +3,40 @@
 (() => {
   let p = $('.sidebar-container'),
     t; // Global scope for 'sidebar-container' and 'sidebar'
-  const _b = (t, p) => { // Observable callback
-    if (window.screen.width <= 576 && p && t) {
-      const b = document.createElement('div');
-      b.className = t.is('[with-backdrop]') ? 'sidebar-backdrop sidebar-overlay' : 'sidebar-overlay';
-      console.log('Checking backdrop: ', b, p.is('.sidebar-toggled'));
-      p.is('.sidebar-toggled') ? p.append(b) : b.parentNode.removeChild(b);
-      b.addEventListener('click', e => {
-        p.removeClass('sidebar-toggled');
-      });
-    }
-  };
-  const _mc = (mt, o) => {
-    for (var m of mt) {
-      if (m.type == 'attributes' && m.attributeName == 'class') {
-        _b(t, p);
-      }
-    }
-  };
-  const _m = new MutationObserver(_mc);
-  for (const _p of p) _m.observe(_p, {
-    attributes: true
-  }); // Observe change in 'sidebar-container' classList
+  // const _b = (t, p) => { // Observable callback
+  //     let b = p.find('.sidebar-overlay');
+  //     if (window.screen.width <= 576 && p && t) {
+  //       if (p.is('.sidebar-toggled')) {
+  //         if (b.length == 0) {
+  //           b = $(document.createElement('div'));
+  //           b.on('click', e => {
+  //             p.removeClass('sidebar-toggled');
+  //           });
+  //           p.append(b);
+  //         }
+  //         b.attr('class', t.is('[with-backdrop]') ? 'sidebar-backdrop sidebar-overlay' : 'sidebar-overlay');
+  //       } else {
+  //         b.remove();
+  //       }
+  //     }
+  // };
+  // const _mc = (mt, o) => {
+  //   for (var m of mt) {
+  //     if (m.type == 'attributes' && m.attributeName == 'class') {
+  //       _b(t, p);
+  //     }
+  //   }
+  // };
+  // const _m = new MutationObserver(_mc);
+  // for (const _p of p) _m.observe(_p, {
+  //   attributes: true
+  // }); // Observe change in 'sidebar-container' classList
+  if (window.screen.width <= 576) $('.page-container').click(e => {
+    if (p && p.is('.sidebar-toggled') && !$(e.target).is('[data-toggle="sidebar"]')) p.removeClass('sidebar-toggled');
+  });
   $('[data-toggle="sidebar"]').click(e => {
-    // t = $($(e.target).data('target')) || $(e.target).parent('.sidebar') || $(e.target).next('.sidebar') || $('.sidebar');
-    t = $('.sidebar');
+    t = $(e.target).data('target') && $($(e.target).data('target')) || !!$(e.target).parent('.sidebar').length && $(e.target).parent('.sidebar') || !!$(e.target).next('.sidebar').length && $(e.target).next('.sidebar');
+    if (!t.length) return;
     p = t.parent('.sidebar-container');
     p.toggleClass('sidebar-toggled');
   }); // Listen for click event on sidebar toggler button
